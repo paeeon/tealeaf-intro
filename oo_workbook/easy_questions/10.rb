617ad1c85f96f4cef5ead711784bfacac50a2ca4
Lily Jen <lily@lilyjen.com>
2015-04-02 12:31:28 -0400

    Removes duplicate exercise files in the easy_questions directory

--- oo_workbook/easy_questions/10.rb
+++
@@ -1,3 +0,0 @@
-# - New level of abstraction to work with 
-# - DRYer code because of the capability of including Mixins and inheritance
-# - Can expose public methods and hide private/protected methods# 
